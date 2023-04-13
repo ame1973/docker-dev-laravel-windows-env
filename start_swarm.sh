bdbec1ff2c94a5ccfc4776365959fe3dc73995fa
HuanXiang <qq418610675@gmail.com>
2023-04-13 12:52:15 +0800

    update swarm

---
+++ start_swarm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "----------------------------------------"
+
+echo "[INFO] START Laravel base env"
+
+echo "Stop docker stacks..."
+docker compose down
+
+echo "Create network..."
+docker network create --driver overlay --attachable --scope swarm service_network || true
+
+echo "Start docker stacks..."
+docker stack deploy -c ./docker-compose.yml base
+
+echo "Done"
