b32c55d7f696ad60f560a0444e3e798ba9aa913f
HuanXiang <qq418610675@gmail.com>
2023-06-05 12:29:16 +0800

    update swarm

--- start_swarm.sh
+++ start_swarm.sh
@@ -8,9 +8,9 @@ echo "Stop docker stacks..."
 docker stack rm base
 
 echo "Create network..."
-docker network create --driver overlay --attachable --scope swarm service_network || true
+docker network create --driver overlay --attachable --scope swarm share_network || true
 
 echo "Start docker stacks..."
-docker stack deploy -c ./docker-compose.yml base
+docker stack deploy -c ./docker-compose_swarm_share.yml base
 
 echo "Done"
